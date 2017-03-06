32ad018e1b41633d252987f79518a9bf071432c5
gooooodok <viktor_88@ukr.net>
2017-03-06 16:18:51 +0200

    php lesson 2: resume + variable

--- Lesson 2_Php_Osnovu/index.php
+++ Lesson 2_Php_Osnovu/index.php
@@ -1,52 +1,194 @@
 <?php
 
-$age = 25;
-$weight = 77.34;
-$name = "Mike";
-$surname = " Johnson";
-$canSwim = false;
-
-$info = "His name is {$name}. His surname is {$surname}. He is {$age} years old.";
+$age = 28;
+$email = "viktor_88@ukr";
+$tel = "+308 (97) 125-88-82";
+$prof = "Designer";
+$name = "Victor";
+$surname = " Zagraychuk";
 
 ?>
+
 
 <!DOCTYPE html>
 <html>
 <head>
-   <meta charset="UTF-8"/>
-   <title>Table</title>
+  <title>
+  Resume - Victor Zagraychuk
+  </title>
+  <meta charset='utf-8'/>
+  <meta name='keywords' content='Resume, Designer, 3D designer'/>
+  <meta name='description' content='Resume Victor Zagraychuk'/>
+  <meta name='author' content='goodok'/>
+  <link href='Resume.css' rel='stylesheet' type='text/css'>
 </head>
+
 <body>
+<div class='container'>
 
-<table border="1">
-   <tr>
-      <th>Name</th>
-      <th>Surname</th>
-      <th>Age</th>
-      <th>Weight</th>
-   </tr>
+  <div class='my_photo'><img class='my_photo' src='img/My photo.jpg' title='This is My photo'/></div>
+  
+    <h1><?= $name . $surname ?></h1>
+	
+	<h3><?= $prof ?></h3>
+	
+	<hr>
+	
+	<h4>Contacts</h4>
+	
+	<div class='contact'>
+	
+	<table>
+	  <tr>
+	    <td><span class='gray'>Date of Birth: &nbsp</span></td>
+	    <td>21.05.1988 (<?= "{$age} years old" ?>)</td>
+	  </tr>
+	  
+	  <tr>
+	    <td><span class='gray'>City:</span></td>
+	    <td>Kiev</td>
+	  </tr>
+	  
+	  <tr>
+	    <td><span class='gray'>Telephone:</span></td>
+	    <td><?= $tel ?></td>
+	  </tr>
+	  
+	  <tr>
+	    <td><span class='gray'>E-mail:</span></td>
+	    <td>viktor_88@ukr.net</td>
+	  </tr>
+	</table>
+	
+	</div>
+	
+  <br>
+	
+  <hr>
+  
+  <h4>Education</h4>
+  
+  <div class='education'>
+	
+	<table>
+	  <tr>
+	    <td><span class='gray'>University: &nbsp</span></td>
+		<td>National Technical University "Kyiv Polytechnic Institute"</td>
+	  </tr>
+	  
+	  <tr>
+	    <td><span class='gray'>Departament:</span></td>
+	    <td>Publishing and printing business</td>
+	  </tr>
+	  
+	  <tr>
+	    <td><span class='gray'>Diploma:</span></td>
+	    <td>Specialist</td>
+	  </tr>
+	  </table>
+  </div>
+  
+  <br>
+  
+  <hr>
+  
+  <h4>Social NetWorks</h4>
+  
+  <div class='networks'>
+    <a href='https://vk.com/id9341975' title='My VK' target='_blank'> <img src='img/VK.jpg' alt='jpg' height='30'/></a>
+    <a href='https://www.drive2.ru/r/chevrolet/958481' title='My Drive3' target='_blank'> <img src='img/Drive2.jpg' alt='jpg' height='30'/></a>
+    <a href='https://www.facebook.com/478929125485733' title='My FaceBook' target='_blank'> <img src='img/FaceBook.jpg' alt='jpg' height='30'/></a>
+  </div>
+  
+  <br>
+  
+  <hr>
+  
+  <h4>Experience</h4>
+  
+  <div class='skill'>
 
-   <tr>   
-      <th><?=$name?></th>   
-      <th><?=$surname?></th>   
-      <th><?=$age?></th>   
-      <th><?=$weight?></th>   
-   </tr>
-</table>
+    <table border='0' cellspacing='5'>
+	  <tr>
+	    <td> <a href='http://lexp.com.ua' target='_blank'>Lombard Expert</a> </td>
+	    <td>
+		  <tr>
+		    <td><span class='gray'>Position:</span></td>
+			<td>Head of marketing department</td>
+		  <tr>
+		</td>
+	  </tr>
+	  
+	  <tr>
+	    <td> <a href='https://www.work.ua/jobs/by-company/644861/' target='_blank'>advertising company "GT-ad"</a> </td>
+	    <td>
+		  <tr>
+		    <td><span class='gray'>Position:</span></td>
+			<td>3D designer</td>
+		  <tr>
+		</td>
+	  </tr>
+	  
+	  <tr>
+	    <td> <a href='http://kuz.ua' target='_blank'>Kyiv Jewelry Factory</a> </td>
+	    <td>
+		  <tr>
+		    <td><span class='gray'>Position:</span></td>
+			<td>Designer</td>
+		  <tr>
+		</td>
+	  </tr>
+	  
+	  <tr>
+	    <td> <a href='https://wolf.ua/?yagla=9680697&gclid=Cj0KEQiA5bvEBRCM6vypnc7QgMkBEiQAUZftQLoJxsqn_8b1bMZTxZyniHky0QhsyjRIIJQhwdyUpJIaAmFh8P8HAQ' target='_blank'>Typography "Wolf"</a> </td>
+	    <td>
+		  <tr>
+		    <td><span class='gray'>Position:</span></td>
+			<td>DTP Designer</td>
+		  <tr>
+		</td>
+	  </tr>
+	  
+	  <tr>
+	    <td>  <a href='http://www.pc-zorya.com.ua/' target='_blank'>Printing plant "Zorya"</a> </td>
+	    <td>
+		  <tr>
+		    <td><span class='gray'>Position:</span></td>
+			<td>DTP Specialist</td>
+		  <tr>
+		</td>
+	  </tr>
+	</table>
+  </div>
+  
+  <br>
+  
+  <hr>
+  
+  <h4>Professional skills</h4>
 
-<?php
+  <div class='Programs_skill'>  
+	
+	<ul> 
+	  <li>HTML, CSS, Git</li>
+      <li> Adobe
+	    <ul>
+          <li> Photoshop </li> 
+          <li> Illustrator </li> 
+          <li> InDezign </li> 
+          <li> Premiere Pro </li> 
+          <li> Audition </li> 
+          <li> Adobe Acrobat Pro (PitStop, Enfocus Prinect)</li> 
+		</ul>  
+	  </li> 
+      <li> 3DsMax (V-ray, Corona)</li> 
+      <li> Corel Draw </li> 
+    </ul>
+  </div>
+  
+  <br>
 
-// notice, warning - предуприждения, не останавливает скрипт.
-// parse error - останавливает скрипт. Где то пропущено ; } " и т.д. 
-// parse error - останавливает скрипт. синтаксически все правельно, но нету например такой переменной, или функции.
-
-// >, <, <=, >=, ==, ===, !=, !==
-
-$x = (int) "12skjvnj5" + 5 + true;
-
-var_dump($x == 18);
-
-?>
-
+</div>	
 </body>
-</html>   
+
+</html>
